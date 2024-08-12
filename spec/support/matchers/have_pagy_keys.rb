a5f7175608e296320f7da6b14998c10efd0fd298
taras viiatyk <tviyatyk@gmail.com>
2024-08-12 13:35:30 +0200

    fix rubocop

--- spec/support/matchers/have_pagy_keys.rb
+++ spec/support/matchers/have_pagy_keys.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec::Matchers.matcher :have_pagy_keys do |expected|
+RSpec::Matchers.matcher :have_pagy_keys do
   match do |actual|
     expect(actual.keys).to eq Pagy::DEFAULT[:metadata]
   end
