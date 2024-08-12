f81e2f9780b5733f9a4e27f67136445a60807ff6
taras viiatyk <tviyatyk@gmail.com>
2024-08-12 02:13:07 +0200

    add CatsController docs

--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -1,20 +1,42 @@
 # frozen_string_literal: true
 
 # CatsController is responsible for handling requests related to the list of cats.
-# It leverages the `Cats::ListOperation` to filter and sort cats based on the provided parameters,
-# and then uses pagination and rendering to deliver the response.
-#
-# The controller action `index` supports the following flow:
-# 1. It calls the `ListOperation` with the request parameters.
-# 2. Depending on the result (Success or Failure), it either renders the cats list
-#    or an error response.
-# 3. If the operation is successful, the result is paginated using `pagy_array`
-#    and rendered using `CatBlueprint`.
-#
-# The controller also includes error handling for unexpected conditions.
 module Api
   module V1
     class CatsController < ApplicationController
+      # Handles the `GET /cats` request.
+      #
+      # CatsController is responsible for handling requests related to the list of cats.
+      # The `index` action retrieves a list of cats based on the provided query parameters,
+      # applies filtering and sorting, paginates the results, and renders them as JSON.
+      #
+      # ### Acceptable Query Parameters:
+      #
+      # - `location` (optional, String): Filters the cats based on their location.
+      #   - Example: `?location=Lviv`
+      #
+      # - `breed` (optional, String): Filters the cats based on their breed.
+      #   - Example: `?breed=Siamese`
+      #
+      # - `sort` (optional, String): Determines the sorting order of the cats based on their price.
+      #   Accepts the following values:
+      #   - `cheap_first`: Sorts the cats in ascending order of price (cheapest first).
+      #   - `expensive_first`: Sorts the cats in descending order of price (most expensive first).
+      #   - Example: `?sort=cheap_first`
+      #
+      # - `limit` (optional, Integer): Limits the number of cats returned in the response.
+      #   - Example: `?limit=10`
+      #
+      # - `page` (optional, Integer): Specifies the page of results to retrieve, in combination with the `limit`.
+      #   - Example: `?page=2`
+      #
+      # ### Example Request:
+      #
+      # `GET /api/v1/cats?location=Lviv&breed=Siamese&sort=cheap_first&limit=1&page=1`
+      #
+      # This example retrieves the cheapest cat of Siamese breed located in Lviv.
+      #
+      # @return [void]
       def index
         case ::Cats::ListOperation.call(params)
         in Success[*cats]
